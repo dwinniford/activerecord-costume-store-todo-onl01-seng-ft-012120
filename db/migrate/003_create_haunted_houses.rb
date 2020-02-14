2106e000567489359041141bbaac7596b3da14fd
Dave Winniford <dwinniford@gmail.com>
2020-02-14 13:08:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,11 +3,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
     create_table :huanted_houses do |t|
       t.string :name 
       t.string :location 
-      t.integer :costume_inventory 
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.string :theme
+      t.float :price 
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.
     end 
   end 
 end 
